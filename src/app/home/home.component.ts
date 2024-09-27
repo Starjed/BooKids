ae1beb4c9c5b315d63c19c944a7f1755890c7ecd
Gabriel Albuquerque <biel123albuquerque@hotmail.com>
2024-09-27 02:18:44 -0400

    Wip seleção de letras com imagem representando

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {SidenavComponent} from "../sidenav/sidenav.component";
+import {WordCardComponent} from "../word-card/word-card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    SidenavComponent
+    SidenavComponent,
+    WordCardComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
