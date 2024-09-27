bdab14236edee27ce91676b788316c29f2ccbee0
Gabriel Albuquerque <biel123albuquerque@hotmail.com>
2024-09-27 01:58:37 -0400

    Adiciona sidenav e importa tailwind para o projeto

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {SidenavComponent} from "../sidenav/sidenav.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    SidenavComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
